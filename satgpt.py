e2bbedd1cc798f011d31f7c4cf5c5ed566747d54
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2023-05-25 17:19:34 -0400

    chore: cleanup, remove prints, and format

--- satgpt.py
+++ satgpt.py
@@ -29,6 +29,7 @@ app = Flask(__name__)
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+
 # TODO: rename from "query" to be more generic, since
 # invoices can be generated for other reasons ("e.g. /add-credit")
 def generate_invoice(query, amount=None):
@@ -38,61 +39,18 @@ def generate_invoice(query, amount=None):
     # result contains the payment request and the r_hash
     return result
 
+
 def check_payment(r_hash):
     invoice = lookup_invoice(r_hash)
     paid = invoice["settled"]
     return (paid, invoice)
 
-# TODO: how can we integrate check-payment into the query endpoint?
-# how does /query know that check_payment has been paid?
-# @app.route('/check-payment', methods=['POST'])
-# def check_payment():
-#     try:
-#         data = request.get_json()
-
-#         if "r_hash" not in data:
-#             response = jsonify({"message": "No query provided"})
-#             return response, 400
-
-#         r_hash = data["r_hash"]
-#         (paid, invoice) = check_payment(r_hash)
-#         if paid:
-#             if check_invoice_used(r_hash):
-#                 response = jsonify({"message": "Payment already used"})
-#                 # What's a good error code that's not an error but doesn't return a response?
-#                 # A neutral code?
-#                 return response, 200
-#             else:
-#                 set_invoice_used(r_hash)
-#                 # deduct credit from user
-#                 user_uuid = lookup_user_by_r_hash(r_hash)
-#                 amount = lookup_invoice(r_hash)["value"]
-#                 set_user_credit(user_uuid, amount, deduct=True)
-
-#             response = jsonify({"message": "Payment successful"})
-#             return response, 200
-#         else:
-#             response = jsonify(
-#                 {
-#                     "message": "Payment Required",
-#                     "payment_request": invoice["payment_request"],
-#                     "memo": invoice["memo"],
-#                 }
-#             )
-#             return response, 402
-#     except Exception as e:
-#         # Return an error response if an exception occurs
-#         error_message = f"An error occurred: {str(e)}"
-#         traceback.print_exc()  # Optional: Print the traceback for debugging purposes
-#         response = {"error": error_message}
-#         return jsonify(response)    
 
 @app.route("/query", methods=["POST"])
 def query_chatbot():
     try:
         # Parse the request data
         data = request.get_json()
-        print("here is the data in TRY ", data)
         # if query is not in data, return error
         if "query" not in data and "user_uuid" not in data and "r_hash" not in data:
             response = jsonify({"message": "No query provided"})
@@ -100,8 +58,7 @@ def query_chatbot():
 
         # check if we're in the middle of a payment
         elif "r_hash" in data:
-            #Is user credit checked here? 
-            print("here is the data in ELIF ", data)
+            # Is user credit checked here?
             r_hash = data["r_hash"]
             (paid, invoice) = check_payment(r_hash)
             if paid:
@@ -150,14 +107,12 @@ def query_chatbot():
                 )
                 return response, 402
         else:
-            
             # If r_hash isn't in request, generate an invoice OR check if user has enough credit
-            print("here is the data in ELSE ", data)
             query = data["query"]
             user_uuid = data["user_uuid"]
-            #TODO: check if user has enough credit
-            '''cur_credit = lookup_user_credit(user_uuid)
-            if cur_credit < price(query):'''
+            # TODO: check if user has enough credit
+            # cur_credit = lookup_user_credit(user_uuid)
+            # if cur_credit < price(query):
 
             # generate an invoice
             invoice = generate_invoice(query)
@@ -165,17 +120,20 @@ def query_chatbot():
             r_hash = base64_to_hex(invoice["r_hash"])
             # add r_hash and query to database
             add_r_hash_and_query(r_hash, query, user_uuid)
-            response = jsonify({
-                "message": "Payment Required",
-                "invoice": invoice["payment_request"],
-                "r_hash": r_hash,
-            })
+            response = jsonify(
+                {
+                    "message": "Payment Required",
+                    "invoice": invoice["payment_request"],
+                    "r_hash": r_hash,
+                }
+            )
             return response, 200
     except Exception as e:
         error_message = f"An error occurred: {str(e)}"
-        traceback.print_exc() 
+        traceback.print_exc()
         response = jsonify({"error": error_message})
         return response, 500
+
 
 @app.route("/add-user", methods=["POST"])
 def add_user():
@@ -195,7 +153,8 @@ def add_user():
         traceback.print_exc()
         response = jsonify({"error": error_message})
         return response, 500
-    
+
+
 @app.route("/get-credit", methods=["POST"])
 def get_credit():
     try:
@@ -227,33 +186,29 @@ def add_credit():
         if "user_uuid" not in data and "amount" not in data and "r_hash" not in data:
             response = jsonify({"message": "No user_uuid or r_hash provided"})
             return response, 400
-        
+
         # check if we're in the middle of a payment
         if "r_hash" in data:
             r_hash = data["r_hash"]
             (paid, invoice) = check_payment(r_hash)
             if paid:
-                # Check that the invoice hasn't been used before
-                print('!*!*!*!*!checking invoice used')
                 if check_invoice_used(r_hash):
-                    # Return the response to the client
-                    # What's a good error code that's not an error but doesn't return a response?
-                    # A neutral code?
                     response = jsonify({"message": "Payment already used"})
-                    return response, 200
+                    return response, 204
                 else:
                     # mark invoice as used
                     set_invoice_used(r_hash)
                     # deduct credit from user
                     user_uuid = lookup_user_by_r_hash(r_hash)
                     amount = lookup_invoice(r_hash)["value"]
-                    print('!*!*!*!*!user id is ', user_uuid, "!*!*!*!*! amount is ", amount)
                     set_user_credit(user_uuid, amount)
-
-                    # Return the response to the client
                     total_credit = lookup_user_credit(user_uuid)
-                    response = jsonify({"message": f'Added {amount} satoshis to user {user_uuid}, total credit is now {total_credit} satoshis.'})
-                    return response, 200 
+                    response = jsonify(
+                        {
+                            "message": f"Added {amount} satoshis to user {user_uuid}, total credit is now {total_credit} satoshis."
+                        }
+                    )
+                    return response, 200
             else:
                 # Return the response to the client
                 response = jsonify(
@@ -274,11 +229,13 @@ def add_credit():
             r_hash = base64_to_hex(invoice["r_hash"])
             # add r_hash and description to database
             add_r_hash_and_query(r_hash, invoice_description, user_uuid)
-            response = jsonify({
-                "message": "Payment Required",
-                "invoice": invoice["payment_request"],
-                "r_hash": r_hash,
-            })
+            response = jsonify(
+                {
+                    "message": "Payment Required",
+                    "invoice": invoice["payment_request"],
+                    "r_hash": r_hash,
+                }
+            )
             return response, 200
     except Exception as e:
         # Return an error response if an exception occurs
@@ -286,35 +243,6 @@ def add_credit():
         traceback.print_exc()
         response = jsonify({"error": error_message})
         return response, 500
-
-# TODO: send file data in request
-# translate audio files (TODO: list which audio types are supported)
-# @app.route('/audio', methods=['GET'])
-# def summarize_audio():
-#     # Parse the request data
-#     data = request.get_json()
-#     query = data['query']
-
-#     # Call the OpenAI API to generate a response
-#     f = open("testfile.mp3", "rb")
-#     transcript = openai.Audio.transcribe("whisper-1", f)
-#     prompt = f"Summarize the following text: {transcript}"
-#     summary = openai.Completion.create(
-#         engine="text-davinci-002",
-#         prompt=prompt,
-#         max_tokens=60,
-#         n=1,
-#         stop=None,
-#         temperature=0.7,
-#     )
-
-
-#     # Extract the response text from the API response
-#     message = summary.choices[0].text.strip()
-
-#     # Return the response to the client
-#     return jsonify({'message': message})
-
 
 # Start the Flask app on localhost:5000
 if __name__ == "__main__":
